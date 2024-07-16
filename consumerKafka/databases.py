e1a61f35a1d2fc6e267d31ef015663545a7dcdcc
Valentin Labrune <valentin.labrune@gmail.com>
2024-07-16 13:57:56 +0200

    stats with mongodb

--- consumerKafka/databases.py
+++ consumerKafka/databases.py
@@ -2,6 +2,8 @@
 from private import password
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import time
+from threading import Thread
 
 class MongoDatabases:
     def __init__(self):
@@ -12,3 +14,64 @@ class MongoDatabases:
     def insertOne(self, collection, data):
         self.db[collection].insert_one(data)
         print(f"Inserted: {data}")
+
+    def retrieveAll(self, collection):
+        documents = self.db[collection].find()
+        all_docs = list(documents)
+        return all_docs
+
+    def processData(self, collection, ope, column):
+        pipeline = [
+            {'$match' : {column: {'$ne': None}}},
+            {'$group': {'_id': None, ope+column: {'$'+ope : '$'+column}}}
+        ]
+        result = list(self.db[collection].aggregate(pipeline))
+        if result:
+            return result[0][ope+column]
+        return None
+
+    def computeAndStoreStats(self, collection, stats_collection):
+        avg_temp = self.processData(collection, 'avg', 'temperature')
+        max_temp = self.processData(collection, 'max', 'temperature')
+        min_temp = self.processData(collection, 'min', 'temperature')
+        avg_humidity = self.processData(collection, 'avg', 'humidity')
+        max_humidity = self.processData(collection, 'max', 'humidity')
+        min_humidity = self.processData(collection, 'min', 'humidity')
+
+        stats = {
+            'average_temperature': avg_temp,
+            'max_temperature': max_temp,
+            'min_temperature': min_temp,
+            'average_humidity': avg_humidity,
+            'max_humidity': max_humidity,
+            'min_humidity': min_humidity,
+            'timestamp': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+        }
+        self.db[stats_collection].insert_one(stats)
+        print(f"Stats updated: {stats}")
+
+
+    def startPeriodicStatsUpdate(self, collection, stats_collection, interval=20):
+        def run():
+            while True:
+                self.computeAndStoreStats(collection, stats_collection)
+                time.sleep(interval)
+
+        thread = Thread(target=run)
+        thread.daemon = True
+        thread.start()
+
+# test = MongoDatabases()
+# print(test.processData('iot_logs', 'avg', 'humidity' ))
+# print(test.processData('iot_logs', 'min', 'temperature'))
+# print(test.computeAndStoreStats('iot_logs', 'iot_stats'))
+# test.startPeriodicStatsUpdate('iot_logs', 'iot_stats')
+
+if __name__ == "__main__":
+    test = MongoDatabases()
+    test.startPeriodicStatsUpdate('iot_logs', 'iot_stats')
+
+    # Keep the main thread alive
+    while True:
+        time.sleep(1)
+
